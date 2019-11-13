b271a05eb4c78b279c8b419e495b20ca54d0c2a4
Aarish Adeel <aarish8@gmail.com>
2019-11-13 00:25:01 -0500

    Start work on routing and navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
+/* Node Dependencies */
 import React from 'react';
-import logo from './images/Hawkshot Logo.svg';
-import './App.css';
-import cards from './data_dragon/en_us/data/set1-en_us'
-import {Container, Row, Col, Image} from 'react-bootstrap'
+import {
+  BrowserRouter as Router,
+  Route, Link, Redirect, withRouter
+} from 'react-router-dom'
 
-function App() {
+/* React-Bootstrap Dependencies */
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+/* Style Dependencies */
+import './App.css';
+
+/* Riot API and DataDragon Dependencies */
+import cards from './data_dragon/en_us/data/set1-en_us'
+
+const TestComponent = ({text}) => {
+  return <p>{text}</p>
+}
+
+
+const App = () => {
   const getCards = (cardType) => {
     const championCards = cards.filter(card => card.type === cardType);
     const divs = championCards.map(card => {
@@ -25,7 +45,32 @@ function App() {
 
   return (
     <div className="App">
-      <header>
+      <Router>
+        <div>
+          <div>
+            <Navbar>
+              <Navbar.Brand as = {Link} to = '/'>Hawkshot</Navbar.Brand>
+              <Nav>
+                <Nav.Link as = {Link} to = '/cards'>Cards</Nav.Link>
+                <Nav.Link as = {Link} to = '/hints'>Hints</Nav.Link>
+              </Nav>
+            </Navbar>
+          </div>
+          <Route exact path = '/' render = {() => <TestComponent text = 'Hawkshot'/>} />
+          <Route exact path = '/cards' render = {() => <TestComponent text = 'Cards'/>} />
+          <Route exact path = '/hints' render = {() => <TestComponent text = 'Hints'/>} />
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+{/* <header>
         <Container>
           <Row>
             <Col sm = {true}>
@@ -71,9 +116,4 @@ function App() {
             {getCards('Spell')}
           </Col>
         </Row>
-      </div>
-    </div>
-  );
-}
-
-export default App;
+      </div> */}
