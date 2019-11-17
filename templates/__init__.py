4ba6348f917e4d2dc11f507740ab43c75aae5013
Bill Li <bill.l1564262@gmail.com>
2019-11-17 13:33:36 -0500

    "sortBy" and "hintId" parameters for GET /api/hints
    Add authentication requirement for POST /api/hints

--- templates/__init__.py
+++ templates/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__,
 static_folder='../client/build/static',
 template_folder='../client/build')
+CORS(app)
 
 import templates.views
