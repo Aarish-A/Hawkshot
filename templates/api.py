281ce924522a8d45746deb14e77daa5977178b1d
Bill Li <bill.l1564262@gmail.com>
2019-11-17 13:33:36 -0500

    "sortBy" and "hintId" parameters for GET /api/hints
    Add authentication requirement for POST /api/hints

--- templates/api.py
+++ templates/api.py
@@ -1,5 +1,6 @@
 import firebase_admin, time
 from firebase_admin import credentials, firestore
+from google.cloud.firestore_v1 import transforms
 
 cred = credentials.Certificate("hawkshot-e7e56-firebase-adminsdk-1zawp-37a5fbdce5.json")
 firebase_admin.initialize_app(cred, {
@@ -25,15 +26,24 @@ def PostHint(data):
 def GetHint(data): #TODO implement all filters
     ref = db.collection(u'hints')
     query = ref
+    query = query.limit(int(data['limit']))
+
     if data['cardId']:
         query = query.where(u'cardId', u'==', data['cardId'])
     if data['ownerId']:
         query = query.where(u'ownerId', u'==', data['ownerId'])
-
-    query = query.limit(int(data['limit']))
+    if data['sortBy']:
+        dir, arg  = data['sortBy'].split('_')
+        if dir == 'a':
+            dir = firestore.Query.ASCENDING
+        else:
+            dir = firestore.Query.DESCENDING
+        query = query.order_by(arg, direction=dir)
+        print(arg, dir)
+    if data['hintId']:
+        query = query.document(data['hintId'])
 
     docs = query.stream()
-    print ('ok boomer')
 
     result = {'hints':[]}
     for doc in docs:
@@ -42,4 +52,15 @@ def GetHint(data): #TODO implement all filters
     return result
 
 def UpdateHint(hintId, type):
+    hint_ref = db.collection(u'hints').document(hintId)
+    if type == 'helpful':
+        hint_ref.update({u'helpful':transforms.Increment(1)})
+    elif type == 'nothelpful':
+        hint_ref.update({u'helpful':transforms.Increment(-1)})
+    elif type == 'funny':
+        hint_ref.update({u'funny':transforms.Increment(1)})
+    elif type == 'notfunny':
+        hint_ref.update({u'funny':transforms.Increment(-1)})
+    else:
+        return Response('failed', 'Invalid type', 401)
     return "bruh"
