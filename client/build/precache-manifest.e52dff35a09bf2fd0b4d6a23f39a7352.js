bcbe79844a7461fc2404423fbc08eb889791d429
Aarish Adeel <aarish8@gmail.com>
2019-11-22 11:19:49 -0500

    Fix build

---
+++ client/build/precache-manifest.e52dff35a09bf2fd0b4d6a23f39a7352.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a985086f0eaccbcfa1f69289740f728",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09fbb6db1217a1f81f1f",
+    "url": "/static/js/2.23f997f1.chunk.js"
+  },
+  {
+    "revision": "b36d5b29d725da0c0739",
+    "url": "/static/js/main.b2037408.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
