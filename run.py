b167e8ff92b7a38026c6e07714f2232223843b4b
Aarish Adeel <aarish8@gmail.com>
2019-11-22 01:33:17 -0500

    Revert "Update run.py"
    This reverts commit bbbf076b5a0d26d5d3c95b5dd4ddf93ae2f280b7.

--- run.py
+++ run.py
@@ -1,7 +1,4 @@
 from templates import app
 #Load this config object for development mode
 app.config.from_object('configurations.DevelopmentConfig')
-
-port = int(os.environ.get('PORT', 33507))
-
-app.run(port=port)
+app.run()
