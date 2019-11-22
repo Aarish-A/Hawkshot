4ccfab75a5c24c89afe250b63df84c16734f2691
Bill Li <bill.l1564262@gmail.com>
2019-11-22 03:02:23 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 from templates import app
 #Load this config object for development mode
 app.config.from_object('configurations.DevelopmentConfig')
-app.run()
+
+port = int(os.environ.get('PORT', 5000))
+
+app.run(port=port)
