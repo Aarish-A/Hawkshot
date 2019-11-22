4ebfb5e23cc78e0bad2f13dd9585a804212dd284
Bill Li <bill.l1564262@gmail.com>
2019-11-22 02:29:08 -0500

    Add support for environment variables

--- templates/api.py
+++ templates/api.py
@@ -4,9 +4,31 @@ from google.cloud.firestore_v1 import transforms
 from flask import Response
 from templates.card_set import card_set
 from profanity_check import predict
+from pathlib import Path
+import os
 
 # initialize firestore
-cred = credentials.Certificate("hawkshot-e7e56-firebase-adminsdk-1zawp-35a7f1dc88.json")
+cert_json = Path("hawkshot-e7e56-firebase-adminsdk-1zawp-35a7f1dc88.json")
+if cert_json.is_file():
+    print("Loading app from file... ")
+    cred = credentials.Certificate("hawkshot-e7e56-firebase-adminsdk-1zawp-35a7f1dc88.json")
+else:
+
+    print("Loading app from environment variables... (this is probably Heroku)")
+    s3 = S3
+    cred = credentials.Certificate({
+        "type": "service_account",
+        "project_id": os.environ.get('PROJECT_ID'),
+        "private_key_id": os.environ.get('PRIVATE_KEY_ID'),
+        "private_key": os.environ.get('PRIVATE_KEY').replace('\\n', '\n'),
+        "client_email": os.environ.get('CLIENT_EMAIL'),
+        "client_id": os.environ.get('CLIENT_ID'),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://accounts.google.com/o/oauth2/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.environ.get('CLIENT_CERT_URL')
+    })
+
 firebase_admin.initialize_app(cred, {
     'projectId': "hawkshot-e7e56",
 })
